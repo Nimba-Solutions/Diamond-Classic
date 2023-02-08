09b3a128deded8c97b1d0eb60f305e18561ccc75
db-lyon <david@bingham.me>
2023-02-07 20:43:18 -0700

    updated apex and lwc

--- force-app/main/default/lwc/pedigreeTable/pedigreeTable.js
+++ force-app/main/default/lwc/pedigreeTable/pedigreeTable.js
@@ -1,87 +1,78 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import { FlowNavigationNextEvent } from 'lightning/flowSupport';
+import createHorsesAndFiles from '@salesforce/apex/Horse.createHorseRecord';
+
 
 export default class PedigreeTable extends LightningElement {
     @api availableActions = [];
-    @api horses = [
-        { 
-            id: 1, 
-            Name: 'test1',
-            Gender: 'Male',
-            DateFoaled: '',
-            Sire: '',
-            Dam: '',
-            Color: '',
-            RegistrationNumber: '1111',
-            AQHA: false,
-            APHA: false,
-            Competing: '',
-            FileIds: []
-        },
-        { 
-            id: 2, 
-            Name: 'test2',
-            Gender: 'Female',
-            DateFoaled: '',
-            Sire: '',
-            Dam: '',
-            Color: '',
-            RegistrationNumber: '22222',
-            AQHA: false,
-            APHA: false,
-            Competing: '',
-            FileIds: []
-        }
-    ];
+    @api horses;
 
     connectedCallback() {
-        this.idIndex = this.horses.length + 1;
+        console.log("HORSES: " + JSON.stringify(this.horses));
+        this.idIndex = this.horses.length;
     }
 
 
     handleAddHorse() {
         const newHorse = {
             id: this.idIndex,
-            Name: '',
-            Gender: '',
-            DateFoaled: '',
-            Sire: '',
-            Dam: '',
-            Color: '',
-            RegistrationNumber: ''
+            Name: null,
+            Gender: null,
+            DateFoaled: null,
+            Sire: null,
+            Dam: null,
+            Color: null,
+            RegistrationNumber: null,
+            AQHA: false,
+            APHA: false,
+            Competing: false,
+            files: null
         };
         this.horses = [...this.horses, newHorse];
         this.idIndex++;
+        console.log("HORSES: " + JSON.stringify(this.horses));
     }
+
 
     handleRemoveHorse(event) {
         this.horses = this.horses.filter(horse => horse.id !== event.detail.horseId);
     }
 
-    handleSubmit() {
+    async handleSubmit() {
         // Get all instances of PedigreeTableRow
         const rows = this.template.querySelectorAll('c-pedigree-table-row');
-        console.log("ROWS: "+ JSON.stringify(rows));
 
-        // Collect data for each horse and its associated files
-        // const data = [];
-        // rows.forEach(row => {
-        //     const horse = row.horse;
-        //     const files = row.uploadedHorse === horse ? row.uploadedFiles.concat(this.uploadedFiles) : row.uploadedFiles;
-        //     console.log("About to push horse & file(s): "+ horse + " : "+ files);
-        //     data.push({ horse, files });
-        // });
-
-        console.log("DATA (ALL): "+ JSON.stringify(this.horses));
+        //Collect data for each horse and its associated files
+        const data = [];
+        rows.forEach(row => {
+            let horse = row.horse;
+            console.log("HORSE: " + JSON.stringify(horse));
+            const files = row.uploadedHorse === horse ? row.uploadedFiles.concat(this.uploadedFiles) : row.uploadedFiles;
+            // Stringify the horse object
+            horse = JSON.stringify(horse);
+            // Add the uploaded files to the horse object
+            let fileIds = files.map(file => `"${file}"`).join(',');
+            horse = horse.replace('"files":null', `"files":[${fileIds}]`);
+            // Parse the horse object back into a JavaScript object
+            horse = JSON.parse(horse);
+            data.push(horse);
+        });
+        this.horses = data;
         
+        try {
+            console.log("CREATING HORSES:"+ JSON.stringify(this.horses));
+           // await createHorsesAndFiles({ horses: this.horses });
+            console.log('Horses created successfully');
+        } catch (error) {
+            console.error('Error creating horses', error);
+        }
+
+
         //handle NEXT
         if (this.availableActions.find((action) => action === 'NEXT')) {
-            // navigate to the next screen
-            console.log("GO NEXT");
             const navigateNextEvent = new FlowNavigationNextEvent();
             this.dispatchEvent(navigateNextEvent);
         }
     }
-
 
 }
--- force-app/main/default/lwc/pedigreeTableRow/pedigreeTableRow.js
+++ force-app/main/default/lwc/pedigreeTableRow/pedigreeTableRow.js
@@ -11,39 +11,38 @@ export default class PedigreeTableRow extends LightningElement {
     { label: 'Female', value: 'Female' }
   ];
   boolOptions = [
-    { label: 'Y', value: 'Y' },
-    { label: 'N', value: 'N' }
+    { label: 'Y', value: true },
+    { label: 'N', value: false }
   ];
 
   handleUploadFinished(event) {
     const uploadedFiles = event.detail.files;
     uploadedFiles.forEach(file => {
-        // Use the file's Id to store it in the horse's Files array
-        this.uploadedFiles.push(file.documentId);
+      // Use the file's Id to store it in the horse's Files array
+      this.uploadedFiles.push(file.documentId);
     });
-    console.log('uploadedFilesId: '+ this.uploadedFiles);
+    console.log('uploadedFilesId: ' + this.uploadedFiles);
   }
-
 
   handleOnChange(event) {
-    if(event.target.type === 'checkbox') {
-      this[event.target.name] = event.target.checked;
-    } else { 
-      this[event.target.name] = event.target.value;
-    }
+    console.log("Event Target Name: "+ JSON.stringify(event.target.name));
+    console.log("Event Target Value: "+ JSON.stringify(event.target.value));
+    console.log("Horse: "+ JSON.stringify(this.horse));
+    // update this.horse
+    this.horse = { ...this.horse, [event.target.name]: event.target.value };
   }
+
 
   handleSearch(event) {
     const lookupElement = event.target;
     apexSearch(event.detail)
-        .then(results => {
-            lookupElement.setSearchResults(results);
-        })
-        .catch(error => {
-            // TODO: handle error
-        });
+      .then(results => {
+        lookupElement.setSearchResults(results);
+      })
+      .catch(error => {
+        // TODO: handle error
+      });
   }
-
 
   removeRow() {
     const removeHorseEvent = new CustomEvent('removehorse', {
